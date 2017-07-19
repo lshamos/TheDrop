68466b92559938f4e80b224794ed1b2b72677660
Lorin Shamos <lshamos@gmail.com>
2017-07-19 11:35:36 -0700

    localhost to 3k not heroku

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
 	'facebookAuth': {
 		'clientID': '1949429758633181',
 		'clientSecret': '1e659506c98155c298307e8dce9a6cdd',
-		'callbackURL': 'https://thedrop.herokuapp.com/auth/facebook/callback',
-		'profileFields': ['emails', 'displayName'] 
+		'callbackURL': 'https://localhost:3000/auth/facebook/callback',
+		'profileFields': ['emails', 'displayName']
 	}
 }
