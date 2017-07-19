e66f5952937ba771cfada653c37b72e27e6439e0
Lorin Shamos <lshamos@gmail.com>
2017-07-19 10:45:58 -0700

    test

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
 		'clientID': '1949429758633181',
 		'clientSecret': '1e659506c98155c298307e8dce9a6cdd',
 		'callbackURL': 'https://thedrop.herokuapp.com/auth/facebook/callback',
-		'profileFields': ['emails', 'displayName']
+		'profileFields': ['emails', 'displayName'] 
 	}
 }
